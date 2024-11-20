b19a0acb787d4cd8f05f27d4e7082e9708f74dd4
Warren Zhang <warrenzhangls@gmail.com>
2024-11-19 22:55:45 -0500

    added post and create to the form

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create'
 end
